b01ee65ed5d4ee65e18781cd92f1fdc427f4d9f9
N.M <90319725+nagymate400@users.noreply.github.com>
2021-10-04 11:39:36 +0200

    Add files via upload

---
+++ FirstApplication/src/main/java/hu/me/iit/webalk/first/ArticleDto.java
@@ -0,0 +1,38 @@
+package hu.me.iit.webalk.first;
+
+public class ArticleDto {
+
+	private String author;
+	private String title;
+	private Integer pages;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getPages() {
+		return pages;
+	}
+
+	public void setPages(Integer pages) {
+		this.pages = pages;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [author=" + author + ", title=" + title + ", pages=" + pages + "]";
+	}
+
+}
