92a3f0e368d9f7f0fa5eb5c532411e16fba6a634
Nagy Máté <nagy.mate@student.uni-miskolc.hu>
2021-10-15 21:20:25 +0200

    6. hét utáni változások.

--- FirstApplication/src/main/java/hu/me/iit/webalk/first/MainController.java
+++ FirstApplication/src/main/java/hu/me/iit/webalk/first/MainController.java
@@ -1,77 +1,40 @@
 package hu.me.iit.webalk.first;
 
-import java.util.ArrayList;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 @RestController
+
 @RequestMapping(path="article")
-public class MainController{
-	private List<ArticleDto> articles = new ArrayList<>();
+public class MainController {
 
-	@GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	private final ArticleService articleService;
 
-	List<ArticleDto> allArtiles() {
-		return articles;
+	public MainController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
 
-	@PostMapping(path = "")
-	public void newArticle(@RequestBody ArticleDto articleDto) {
-		articles.add(articleDto);
-	}
-	
-	@PutMapping(path = "{id}")
-	public void replaceArticle(@PathVariable("id") String id, @RequestBody ArticleDto articleDto) {
-	}
-	
-	@DeleteMapping(path = "{id}")
-	public void deleteArticle(@PathVariable("id") String id) {
-		int found = -1;
-		for (int i = 0 ; i <  articles.size(); i++) {
-			if (articles.get(i).getTitle().equals(id)) {
-				found = i;
-				break;
-			}
-		}
-	}
-		
-		private int FindArticle(@PathVariable("id") String id) {
-			int found = -1;
-			for (int i = 0 ; i <  articles.size(); i++) {
-				if (articles.get(i).getTitle().equals(id)) {
-					found = i;
-					break;
-				}
-			}
-		return found;	
-		
-		if (found != -1) {
-			ArticleDto foundArticle = articles.get(found);
-			foundArticle.setAuthor(articleDto.getAuthor());
-			foundArticle.setPages(articleDto.getPages());
-			
-		}
+	@GetMapping(path="", produces= MediaType.APPLICATION_JSON_VALUE)
+	public List<ArticleDto> allArticles() {
+		return articleService.findAll();
 	}
 
-	@ResponseBody
-	ResponseDto gyoker() {
-		ResponseDto responseDto = new ResponseDto();
+	@PostMapping(path="")
+	public void newArticle(@RequestBody @Valid ArticleDto articleDto) {
+		articleService.save(articleDto);
+	}
 
-		responseDto.setId(123);
-		responseDto.setMessage("hello");
-		return responseDto;
+	@PutMapping(path="/")
+	public void replaceArticle(@RequestBody @Valid ArticleDto articleDto) {
+		articleService.save(articleDto);
+	}
+
+	@DeleteMapping (path="/{id}")
+	public void deleteArticle(@PathVariable("id") Long id) {
+		articleService.deleteById(id);
 	}
 
 }
---
+++ FirstApplication/src/test/java/hu/me/iit/webalk/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.first;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
